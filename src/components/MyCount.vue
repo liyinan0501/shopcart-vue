185e45ae8650403af2eae3aeedfdb5920fb63723
Yinan Li <gabriel.li5566@gmail.com>
2022-12-30 20:32:29 +0200

    project init

---
+++ src/components/MyCount.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my-counter">
+    <button type="button" class="btn btn-light">-</button>
+    <input type="number" class="form-control inp" />
+    <button type="button" class="btn btn-light">+</button>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.my-counter {
+  display: flex;
+  .inp {
+    width: 45px;
+    text-align: center;
+    margin: 0 10px;
+  }
+  .btn,
+  .inp {
+    transform: scale(0.9);
+  }
+}
+</style>
