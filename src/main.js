f860d9b8ef162027eb0f6f75ec4710b7004395c2
Yinan Li <gabriel.li5566@gmail.com>
2023-01-03 21:12:21 +0200

    configure axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'https://www.escook.cn'
+const app = createApp(App)
+app.config.globalProperties.$axios = axios
+app.mount('#app')
